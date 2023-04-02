9da5a52db9ee8ff37b9acf5793e03812c22bc951
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-04-02 07:53:30 +0000

    [update] model association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
   has_one_attached :image
+  
+  has_many :favorite, dependent: :destroy
+  has_many :posts_coment, dependent: :destroy
+  belongs_to :customer
+  belongs_to :shop_info
+  belongs_to :genre
+  
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/image/no-image.png')
+      image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/png')
+    end
+    image
+  end
 end
