aec08f243f1e5686314f8e3369568998521d321f
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-04-11 14:49:23 +0000

    [wip] favorite post show view

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,15 @@
 class Public::FavoritesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.new(post_id: post.id)
-    favorite.saave
+    posts = Post.find(params[:post_id])
+    favorite = current_customer.favorites.new(post_id: posts.id)
+    favorite.save
     redirect_to post_path(post)
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.find_by(post_id: post.id)
+    posts = Post.find(params[:post_id])
+    favorite = current_customer.favorites.find_by(post_id: posts.id)
     favorite.destroy
-    redirect_to Post_path(post)
+    redirect_to post_path(post)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,9 @@ class Post < ApplicationRecord
   has_many :genres, -> { order(:name) }, through: :post_genres
   belongs_to :customer
   belongs_to :shop_info
-  has_many :favorite_posts, through: :favorites, source: :post
+  has_many :favorites, dependent: :destroy
   
+
   def get_image(width, hight)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no-image.png')
@@ -16,7 +17,7 @@ class Post < ApplicationRecord
       image.variant(resize_to_limit: [width, hight]).processed
     end
   end
-  
+
   validates :rate, numericality: {
     less_than_or_equal_to: 5,
     greater_than_or_equal_to: 0
@@ -25,6 +26,7 @@ class Post < ApplicationRecord
   def favorited_by?(customer)
     favorites.exists?(customer_id: customer.id)
   end
-  
+
+
 
 end
