4df3b9db9f5b9322bdb047cb300e9d6d77029462
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-05-04 15:30:32 +0000

    [update] public top view

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,8 @@ class Public::HomesController < ApplicationController
   def top
     if customer_signed_in?
       @customer = Customer.find(current_customer.id)
+      @shop_infos = ShopInfo.all
+      @genres = Genre.all
     end
   end
 end
