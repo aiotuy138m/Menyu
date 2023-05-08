eb4188adffdd9d72c8e037c2e954eca76e4ddd71
aiotuy138m <122207424+aiotuy138m@users.noreply.github.com>
2023-05-08 14:42:04 +0000

    [wip] admin report index view

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,10 @@
 class Report < ApplicationRecord
   belongs_to :reporter, class_name: "Post", optional: true
   belongs_to :reported, class_name: "Post", optional: true
+  
+  
+  def reported_customer
+    @posts = Post.where(id: reported_id)
+    @customer = Customer.where(id: @posts.customer_id)
+  end
 end
